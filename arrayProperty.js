12cc16a0ced3f01c471bbe98409c1a940694076a
Allen <agvawd@hotmail.com>
2015-01-21 17:51:19 -0700

    Did all coding exercises

--- arrayProperty.js
+++ arrayProperty.js
@@ -3,3 +3,8 @@
 //Add a reverse method to the String 'class' so that every instance of String can call reverse and reverse itself.
   
   //code here
+  String.reverseStr = function(str) {
+  	return str.split("").reverse().join("");
+  }
+
+  String.reverseStr("Allen");
--- instanceArray.js
+++ instanceArray.js
@@ -14,28 +14,37 @@ var User = function(name, email, pw){
 //Create an Array called 'users' that will store all our instances of User.
 
   //code here
-
+var users = [];
+var Tyler = new User('Tyler', 'tylermcginnis33@gmail.com', 'iLoveJS');
+var Cahlan = new User('Cahlan', 'cahlan@devmounta.in', 'iLoveHashtags');
+var Lenny = new User('Lenny', 'lenny@theLenster.com', 'iLoveLentilSoup');
 
 //Now create and push into your users array 3 seperate instances of User using the data from above in that exact order
 
   //code here
+  users.push(Tyler, Cahlan, Lenny);
 
-console.log('Tyler\'s information is ');
 //Console.log all of Tylers information
 
   //code here
+console.log('Tyler\'s information is ', Tyler);
 
-console.log('Lenny\'s information is ');
 //Now console.log all of Lennys information
 
   //code here
+console.log('Lenny\'s information is ', Lenny);
 
 
 //Now create another instance of User using your own information and then add that to your users array.
 
   //code here
+  var Allen = new User('Allen', 'agvawd@hotmail.com', 'whoseAsking1');
 
-console.log('All my users names are ');
+console.log('All my users names are ... ');
 //Now loop through your users Array and console.log every users name. 
+users.push(Allen);
 
   //code here
+  for (var i = 0; i < users.length; i++) {
+    console.log(users[i].name);
+  }
